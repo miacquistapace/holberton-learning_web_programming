d1daf4f34dddab0af4f3ba62391dc50686ce9c96
miacquistapace <mishaman99@gmail.com>
2017-06-30 12:25:35 -0700

    fixed push

--- 5-javascript/102-array.js
+++ 5-javascript/102-array.js
@@ -1,10 +1,11 @@
 var dwarves ={'Doc', 'Grumpy', 'Happy', 'Sleepy','Dopey','Bashful','Sneezy'};
 
 function equal(){
-  var nums = [50]
+  var nums = []
+
   for (let i=2;i <=100;i++)
    if (i % 2 == 0)
-      nums.add(i);
+      nums.push(i);
 
   return nums;
 }
