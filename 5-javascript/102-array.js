ee77f22dd40b95006449014fb2389efe3a20d01b
miacquistapace <mishaman99@gmail.com>
2017-06-30 11:24:16 -0700

    arrays

---
+++ 5-javascript/102-array.js
@@ -0,0 +1,10 @@
+var dwarves ={'Doc', 'Grumpy', 'Happy', 'Sleepy','Dopey','Bashful','Sneezy'};
+
+function equal(){
+  var nums = [50]
+  for (let i=2;i <=100;i++)
+   if (i % 2 == 0)
+      nums.add(i);
+
+  return nums;
+}
